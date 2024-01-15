7589db44fee69ff6c4c55aa616ab8c39c1e0e5cf
garrettladley <ladley.g@northeastern.edu>
2024-01-15 13:41:03 -0500

    init data.sql updates | club bug fix after enum change

--- backend/src/migrations/data.sql
+++ backend/src/migrations/data.sql
@@ -1,1 +1,23 @@
+-- BEGIN SUPER CREATION TRANSACTION
+BEGIN;
+
+-- SUPER USER
+INSERT INTO users (role, nuid, email, password_hash, first_name, last_name, college, year) VALUES ('super', '000000000', 'generatesac@gmail.com', 'rust', 'SAC', 'Super', 'KCCS', 1);
+
+-- SAC SUPER CLUB
+INSERT INTO clubs (name, preview, description, num_members, logo) VALUES ('SAC', 'SAC', 'SAC', 1, 'foo');
+
+-- INSERT SUPER USER INTO SAC SUPER CLUB
+INSERT INTO user_club_members (club_id, user_id) VALUES (1, 1);
+
+COMMIT;
+-- END SUPER CREATION TRANSACTION
+
+-- BEGIN MOCK DATA TRANSACTION
+BEGIN;
+
 INSERT INTO users (role, nuid, email, password_hash, first_name, last_name, college, year) VALUES ('super', '002183108', 'oduneye.d@northeastern.edu', 'rust', 'David', 'Oduneye', 'KCCS', 3);
+INSERT INTO users (role, nuid, email, password_hash, first_name, last_name, college, year) VALUES ('super', '002172052', 'ladley.g@northeastern.edu', 'rust', 'Garrett', 'Ladley', 'KCCS', 3);
+
+COMMIT;
+-- END MOCK DATA TRANSACTION
