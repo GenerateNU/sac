6de9c6ecda5db52e43d64a653564d6119c00ed9b
Michael Brennan <michael.brennancolorado@gmail.com>
2024-01-19 16:09:11 -0500

    feat: PATCH UpdateUser - redo validation to use go-validator library

--- backend/src/services/user.go
+++ backend/src/services/user.go
@@ -1,8 +1,6 @@
 package services
 
 import (
-	"errors"
-
 	"github.com/GenerateNU/sac/backend/src/models"
 	"github.com/GenerateNU/sac/backend/src/transactions"
 	"github.com/GenerateNU/sac/backend/src/types"
@@ -14,8 +12,7 @@ import (
 )
 
 type UserServiceInterface interface {
-	ValidateUserParams(params types.UserParams, noEmptyFields bool) error
-	CreateUserFromParams(params types.UserParams) models.User
+	CreateUserFromParams(params types.UserParams) (models.User, error)
 	GetAllUsers() ([]models.User, error)
 	GetUser(id string) (*models.User, error)
 	UpdateUser(id string, params types.UserParams) (models.User, error)
@@ -26,57 +23,16 @@ type UserService struct {
 	Validate *validator.Validate
 }
 
-// Validates the fields of params, and returns an error if any field is invalid. If noEmptyFields is true,
-// then an error will be thrown if any field is missing.
-func (u *UserService) ValidateUserParams(params types.UserParams, noEmptyFields bool) error {
-	// check for empty fields
-	if noEmptyFields {
-		if params.NUID == "" {
-			return errors.New("nuid is missing")
-		}
-		if params.FirstName == "" {
-			return errors.New("first name is missing")
-		}
-		if params.LastName == "" {
-			return errors.New("last name is missing")
-		}
-		if params.Email == "" {
-			return errors.New("email is missing")
-		}
-		if params.Password == "" {
-			return errors.New("password is missing")
-		}
-		if params.College == "" {
-			return errors.New("college is missing")
-		}
-		if params.Year == 0 {
-			return errors.New("year is missing")
-		}
-	}
-
-	// run validation rules
-	if err := utilities.ValidateNUID(params.NUID); params.NUID != "" && err != nil {
-		return err
-	}
-	if err := utilities.ValidateEmail(params.Email); params.Email != "" && err != nil {
-		return err
-	}
-	if err := utilities.ValidatePassword(params.Password); params.Password != "" && err != nil {
-		return err
-	}
-	if err := utilities.ValidateCollege(params.College); params.College != "" && err != nil {
-		return err
-	}
-	if err := utilities.ValidateYear(params.Year); params.Year != 0 && err != nil {
-		return err
-	}
-
-	return nil
-}
-
 // Creates a models.User from params. This *does not* interact with the database at all; the value will need to be
 // passed to gorm.Db.Create(interface{}) for it to be persisted.
-func (u *UserService) CreateUserFromParams(params types.UserParams) models.User {
+func (u *UserService) CreateUserFromParams(params types.UserParams) (models.User, error) {
+	validate := validator.New()
+	validate.RegisterValidation("neu_email", utilities.ValidateEmail)
+	validate.RegisterValidation("password", utilities.ValidatePassword)
+	if err := validate.Struct(params); err != nil {
+		return models.User{}, fiber.NewError(fiber.StatusBadRequest, err.Error())
+	}
+
 	var user models.User
 	user.NUID = params.NUID
 	user.FirstName = params.FirstName
@@ -87,7 +43,7 @@ func (u *UserService) CreateUserFromParams(params types.UserParams) models.User 
 	user.College = models.College(params.College)
 	user.Year = models.Year(params.Year)
 
-	return user
+	return user, nil
 }
 
 // Gets all users (including soft deleted users) for testing
@@ -107,9 +63,10 @@ func (u *UserService) GetUser(userID string) (*models.User, error) {
 
 // Updates a user
 func (u *UserService) UpdateUser(id string, params types.UserParams) (models.User, error) {
-	if err := u.ValidateUserParams(params, false); err != nil {
-		return models.User{}, fiber.NewError(fiber.StatusBadRequest, err.Error())
+	user, err := u.CreateUserFromParams(params)
+	if err != nil {
+		return models.User{}, err
 	}
 
-	return transactions.UpdateUser(u.DB, id, u.CreateUserFromParams(params))
+	return transactions.UpdateUser(u.DB, id, user)
 }
