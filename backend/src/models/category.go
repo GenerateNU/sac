ae5e0c97371ef947b05f6c23a03c1cdd2847d00c
garrettladley <ladley.g@northeastern.edu>
2024-01-19 16:10:08 -0500

    test

--- backend/src/models/category.go
+++ backend/src/models/category.go
@@ -4,7 +4,6 @@ import "github.com/GenerateNU/sac/backend/src/types"
 
 type Category struct {
 	types.Model
-
 	Name string `gorm:"type:varchar(255)" json:"category_name" validate:"required,max=255"`
 
 	Tag []Tag `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"-" validate:"-"`
