9539a208909aab0ef131938b4a73fede57511c81
garrettladley <ladley.g@northeastern.edu>
2024-01-18 20:27:01 -0500

    RequestTestSetup helper | drop fiber context from validator

--- backend/tests/api/user_test.go
+++ backend/tests/api/user_test.go
@@ -3,31 +3,20 @@ package tests
 import (
 	"backend/src/models"
 	"backend/src/transactions"
-	"fmt"
-	"net/http/httptest"
 	"testing"
 
 	"github.com/goccy/go-json"
 )
 
 func TestGetAllUsersWorks(t *testing.T) {
-	// initialize the test
-	app, assert := InitTest(t)
-
-	// create a GET request to the APP/api/v1/users/ endpoint
-	req := httptest.NewRequest("GET", fmt.Sprintf("%s/api/v1/users/", app.Address), nil)
-
-	// send the request to the app
-	resp, err := app.App.Test(req)
-
-	assert.NilError(err)
+	app, assert, resp := RequestTestSetup(t, "GET", "/api/v1/users/", nil)
 
 	assert.Equal(200, resp.StatusCode)
 
 	// decode the response body into a slice of users
 	var users []models.User
 
-	err = json.NewDecoder(resp.Body).Decode(&users)
+	err := json.NewDecoder(resp.Body).Decode(&users)
 
 	assert.NilError(err)
 
