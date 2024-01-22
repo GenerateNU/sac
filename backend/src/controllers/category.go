90f54819965a7b62e9a302ed90f4226a51586bac
Alder Whiteford <alderjwhiteford@gmail.com>
2024-01-22 14:14:40 -0500

    Update backend/src/controllers/category.go
    Co-authored-by: Garrett Ladley <92384606+garrettladley@users.noreply.github.com>

--- backend/src/controllers/category.go
+++ backend/src/controllers/category.go
@@ -29,7 +29,7 @@ func NewCategoryController(categoryService services.CategoryServiceInterface) *C
 // @Failure     500   {string}    string "failed to create category"
 // @Router		/api/v1/category/  [post]
 func (t *CategoryController) CreateCategory(c *fiber.Ctx) error {
-	var category models.CreateUpdateCategoryRequestBody
+	var category models.CreateCategoryRequestBody
 
 	if err := c.BodyParser(&category); err != nil {
 		return fiber.NewError(fiber.StatusBadRequest, "failed to process the request")
