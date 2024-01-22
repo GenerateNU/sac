83bb7cf99d54ea9096f54d36b7e870970d220ee1
Michael Brennan <76018881+michael-brennan2005@users.noreply.github.com>
2024-01-22 14:12:51 -0500

    Validator singleton (#43)

--- backend/src/services/category.go
+++ backend/src/services/category.go
@@ -3,7 +3,7 @@ package services
 import (
 	"github.com/GenerateNU/sac/backend/src/models"
 	"github.com/GenerateNU/sac/backend/src/transactions"
-	"github.com/GenerateNU/sac/backend/src/utilities"
+	"github.com/go-playground/validator/v10"
 
 	"github.com/gofiber/fiber/v2"
 	"golang.org/x/text/cases"
@@ -16,11 +16,12 @@ type CategoryServiceInterface interface {
 }
 
 type CategoryService struct {
-	DB *gorm.DB
+	DB       *gorm.DB
+	Validate *validator.Validate
 }
 
 func (c *CategoryService) CreateCategory(category models.Category) (*models.Category, error) {
-	if err := utilities.ValidateData(category); err != nil {
+	if err := c.Validate.Struct(category); err != nil {
 		return nil, fiber.NewError(fiber.StatusBadRequest, "failed to validate the data")
 	}
 
