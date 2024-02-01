bd4c35411a46dbf0a16104bdbf8503f40a3b402e
David Oduneye <44040421+DOOduneye@users.noreply.github.com>
2024-02-01 14:53:07 -0500

    Update backend/src/models/membership.go
    Co-authored-by: Garrett Ladley <92384606+garrettladley@users.noreply.github.com>

--- backend/src/models/membership.go
+++ backend/src/models/membership.go
@@ -21,7 +21,7 @@ type Membership struct {
 	Model
 
 	ClubID uuid.UUID `gorm:"primaryKey" json:"club_id" validate:"required,uuid4`
-	UserID uuid.UUID `gorm:"primaryKey" json:"user_id" validate:"required,min=1"`
+	UserID uuid.UUID `gorm:"primaryKey" json:"user_id" validate:"required,uuid4"`
 	Type MembershipType `gorm:"type:varchar(255);default:member" json:"membership_type" validate:"required,max=255"`
 
 	Club *Club `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"-" validate:"-"`
