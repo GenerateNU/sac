2522cb4ff1176ed46029c6a69af6806d4b43dff3
Alder Whiteford <alderwhiteford@Alders-MacBook-Pro-2.local>
2024-02-18 22:52:50 -0500

    More linting stuff

--- backend/src/utilities/response.go
+++ backend/src/utilities/response.go
@@ -4,7 +4,7 @@ import "github.com/gofiber/fiber/v2"
 
 // For swagger docs:
 type SuccessResponse struct {
-	Message string `json:message`
+	Message string `json:"message"`
 }
 
 func FiberMessage(c *fiber.Ctx, statusCode int, response string) error {
