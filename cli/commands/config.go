c03963db70842d42d9ff135e5aec0e5f502731fd
Alder Whiteford <alderwhiteford@Alders-MacBook-Pro-2.local>
2024-03-05 19:24:40 -0500

    Formatting to CLI files

--- cli/commands/config.go
+++ cli/commands/config.go
@@ -8,9 +8,9 @@ import (
 )
 
 var (
-	ROOT_DIR, _    		= utils.GetRootDir()
-	FRONTEND_DIR   		= filepath.Join(ROOT_DIR, "/frontend")
-	BACKEND_DIR    		= filepath.Join(ROOT_DIR, "/backend/")
-	CONFIG, _      		= config.GetConfiguration(filepath.Join(ROOT_DIR, "/config"), false)
-	MIGRATION_FILE      = filepath.Join(BACKEND_DIR, "/migrations/data.sql")
+	ROOT_DIR, _    = utils.GetRootDir()
+	FRONTEND_DIR   = filepath.Join(ROOT_DIR, "/frontend")
+	BACKEND_DIR    = filepath.Join(ROOT_DIR, "/backend/")
+	CONFIG, _      = config.GetConfiguration(filepath.Join(ROOT_DIR, "/config"), false)
+	MIGRATION_FILE = filepath.Join(BACKEND_DIR, "/migrations/data.sql")
 )
