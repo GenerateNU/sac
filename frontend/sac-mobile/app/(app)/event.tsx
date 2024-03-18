1032c3bcb78a88028d276ec684633d56b0197ec1
garrettladley <ladley.g@northeastern.edu>
2024-03-18 10:52:08 -0400

    remove unused dep

--- frontend/sac-mobile/app/(app)/event.tsx
+++ frontend/sac-mobile/app/(app)/event.tsx
@@ -7,7 +7,14 @@ const Event = () => {
     const { data: event, isLoading, error } = useEvent('tester');
 
     // TODO: Handle error once we have error components
+    if (error) {
+        console.error(error);
+    }
+
     // TODO: Handle loading state once we have loading components
+    if (isLoading) {
+        return <Text>Loading...</Text>;
+    }
 
     return (
         <>
