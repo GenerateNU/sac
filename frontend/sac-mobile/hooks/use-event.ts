3c08e6c7b6e4f1f23064f290f78d57934494820b
garrettladley <ladley.g@northeastern.edu>
2024-03-24 18:59:47 -0400

    de -> te

--- frontend/sac-mobile/hooks/use-event.ts
+++ frontend/sac-mobile/hooks/use-event.ts
@@ -1,14 +1,13 @@
-import { UseQueryResult, useQuery } from '@tanstack/react-query';
-
 import { fetchEvent } from '@/services/event';
 import { Event } from '@/types/event';
 import { uuid } from '@/types/uuid';
+import { useQuery, UseQueryResult } from '@tanstack/react-query';
 
 export const useEvent = (eventID: uuid): UseQueryResult<Event, Error> => {
     return useQuery({
         queryKey: ['event', eventID],
         queryFn: () => {
-            if (eventID == 'tester') {
+            if (eventID === 'tester') {
                 return {
                     id: 'tester',
                     name: 'Tester Event',
